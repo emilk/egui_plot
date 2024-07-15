a40db950d074ee1f381ef528896b058652d8c523
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-07-15 17:45:52 +0200

    Add docs to demo

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,3 +1,7 @@
+//! Demo code for the [`egui_plot`], hosted at <https://emilk.github.io/egui_plot/>.
+//!
+//! Each push to `main` re-deploys the demo.
+
 #![warn(clippy::all, rust_2018_idioms)]
 
 mod app;
