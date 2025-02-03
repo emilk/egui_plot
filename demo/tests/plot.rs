00707d1dd9b8665b22435bf00a5008ac2344b08d
Lucas Meurer <lucasmeurer96@gmail.com>
2025-02-03 16:33:00 +0100

    Add kittest snapshot tests

---
+++ demo/tests/plot.rs
@@ -0,0 +1,57 @@
+use demo::TemplateApp;
+use egui::accesskit::Role;
+use egui::ThemePreference;
+use egui_kittest::kittest::Queryable;
+
+#[test]
+fn test_demos() {
+    let mut harness =
+        egui_kittest::Harness::new_eframe(|cc| TemplateApp::new(cc));
+
+    let demo_names: Vec<_> = harness
+        .get_by_role_and_label(Role::RadioGroup, "Select Demo")
+        .get_all_by_role(Role::Button)
+        .filter_map(|w| w.label())
+        .collect();
+
+    let mut errors = Vec::new();
+
+    for name in demo_names {
+        harness.get_by_label(&name).click();
+        harness.run();
+
+        if let Err(error) = harness.try_snapshot(&format!("demos/{}", name)) {
+            errors.push(error);
+        }
+    }
+
+    if !errors.is_empty() {
+        panic!("Errors: {:#?}", errors);
+    }
+}
+
+#[test]
+fn test_scales() {
+    for scale in [0.5, 1.0, 1.39, 2.0] {
+        let mut harness = egui_kittest::HarnessBuilder::default()
+            .with_pixels_per_point(scale)
+            .build_eframe(|cc| TemplateApp::new(cc));
+
+        harness.run();
+
+        if let Err(error) = harness.try_snapshot(&format!("scale_{:.2}", scale)) {
+            panic!("{}", error);
+        }
+    }
+}
+
+#[test]
+fn test_light_mode() {
+    let mut harness =
+        egui_kittest::Harness::new_eframe(|cc| TemplateApp::new(cc));
+
+    harness.ctx.set_theme(ThemePreference::Light);
+    harness.run();
+
+    harness.snapshot("light_mode");
+}
