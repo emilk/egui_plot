0e683dd7f40371effff42fb12fcd4757d4428a54
Lucas Meurer <lucasmeurer96@gmail.com>
2025-02-05 08:40:53 +0100

    Update snapshots after rebase

--- egui_plot/src/lib.rs
+++ egui_plot/src/lib.rs
@@ -19,9 +19,8 @@ use std::{cmp::Ordering, ops::RangeInclusive, sync::Arc};
 
 use ahash::HashMap;
 use egui::{
-    epaint, remap_clamp, vec2, Align2, Color32,  CursorIcon, Id, Layout, NumExt,
-    PointerButton, Pos2, Rangef, Rect, Response, Sense, Shape, Stroke, TextStyle, Ui, Vec2, Vec2b,
-    WidgetText,
+    epaint, remap_clamp, vec2, Align2, Color32, CursorIcon, Id, Layout, NumExt, PointerButton,
+    Pos2, Rangef, Rect, Response, Sense, Shape, Stroke, TextStyle, Ui, Vec2, Vec2b, WidgetText,
 };
 use emath::Float as _;
 
