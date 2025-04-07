emilk/egui_plot

commit 7dcb501f08c47258e76088fca85f87279cf60b91

Fix include_{xy} issue when auto bounds is off (#74)
<!--
Please read the "Making a PR" section of
[`CONTRIBUTING.md`](https://github.com/emilk/egui_plot/blob/master/CONTRIBUTING.md)
before opening a Pull Request!

* Keep your PR:s small and focused.
* The PR title is what ends up in the changelog, so make it descriptive!
* If applicable, add a screenshot or gif.
* If it is a non-trivial addition, consider adding a demo f or a new
example.
* Do NOT open PR:s from your `master` or `main` branch, as that makes it
hard for maintainers to test and add commits to your PR.
* Remember to run `cargo fmt` and `cargo clippy`.
* Open the PR as a draft until you have self-reviewed it and run
`./scripts/check.sh`.
* When you have addressed a PR comment, mark it as resolved.

Please be patient! We will review your PR, but our time is limited!
-->
Re-open the following PR originally open by @aholtzma-am:
https://github.com/emilk/egui/pull/4539#issuecomment-2164776123

> The logic for determining bounds was not using the values supplied by
include_x()/include_y() after the initial plot call when auto_bounds()
was false. This change forces the use of the manual limits when
default_auto_bounds is false.

Closes https://github.com/emilk/egui_plot/issues/25

I did not try the suggested approach
(https://github.com/emilk/egui/pull/4539#issuecomment-2164776123), as I
would need some more in-depth understanding before risking breaking
other stuff.

Maybe, with some support, I could try to implement that approach in a
separate PR.

egui_plot/src/lib.rs
@@ -985,10 +985,10 @@ impl<'a> Plot<'a> {
         }
 
         // Reset bounds to initial bounds if they haven't been modified.
-        if mem.auto_bounds.x {
+        if !default_auto_bounds.x || mem.auto_bounds.x {
             bounds.set_x(&min_auto_bounds);
         }
-        if mem.auto_bounds.y {
+        if !default_auto_bounds.y || mem.auto_bounds.y {
             bounds.set_y(&min_auto_bounds);
         }
 

commit 552d13ee438fa9d874e2c0a3b4587597591a2cfb

Fix dynamic bounds modifications (#96)
* This broke in https://github.com/emilk/egui_plot/pull/74

The problem was caused by using `PlotUi::set_auto_bounds`. I don't fully
understand it all.

I really don't like this. The code is confusing and lacking of tests,
but as usual, I don't have time to dig into it right now.

egui_plot/src/lib.rs
@@ -994,6 +994,7 @@ impl<'a> Plot<'a> {
             mem.auto_bounds = true.into();
         }
 
+        let any_dynamic_modifications = !bounds_modifications.is_empty();
         // Apply bounds modifications.
         for modification in bounds_modifications {
             match modification {
@@ -1017,10 +1018,10 @@ impl<'a> Plot<'a> {
         }
 
         // Reset bounds to initial bounds if they haven't been modified.
-        if !default_auto_bounds.x || mem.auto_bounds.x {
+        if (!default_auto_bounds.x && !any_dynamic_modifications) || mem.auto_bounds.x {
             bounds.set_x(&min_auto_bounds);
         }
-        if !default_auto_bounds.y || mem.auto_bounds.y {
+        if (!default_auto_bounds.y && !any_dynamic_modifications) || mem.auto_bounds.y {
             bounds.set_y(&min_auto_bounds);
         }
 