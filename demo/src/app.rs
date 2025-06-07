f7013a7e307dbd70f5e649140bf496783beeb1d6
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-06-07 20:12:02 +0200

    Update to latest egui trunk (#111)
    No breaking changes so far, but there are new shortcuts for zooming the
    X/Y axes independently, so I've added instructions for these (that
    depend on trunk)

--- demo/src/app.rs
+++ demo/src/app.rs
@@ -32,7 +32,7 @@ impl eframe::App for TemplateApp {
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
 
-            egui::menu::bar(ui, |ui| {
+            egui::containers::menu::Bar::new().ui(ui, |ui| {
                 egui::widgets::global_theme_preference_buttons(ui);
 
                 ui.add_space(16.0);
