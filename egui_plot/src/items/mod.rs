6fe4e40b5d2efdf70b15459233aa97d91dff4e73
Ygor Souza <ygor.souza@protonmail.com>
2025-08-07 06:52:49 +0200

    Clarify the relationship between name and id
    The name passed to new() defines the initial id, but changing it with
    name() does not change the id, it has to be changed explicitly with
    id(). This documentation update attempts to make this caveat more
    explicit, as it is not apparent from the public API without looking at
    the source.

--- egui_plot/src/items/mod.rs
+++ egui_plot/src/items/mod.rs
@@ -54,6 +54,11 @@ macro_rules! builder_methods_for_base {
         /// Name of this plot item.
         ///
         /// This name will show up in the plot legend, if legends are turned on.
+        ///
+        /// Setting the name via this method does not change the item's id, so you can use it to
+        /// change the name dynamically between frames without losing the item's state. You should
+        /// make sure the name passed to [`Self::new`] is unique and stable for each item, or
+        /// set unique and stable ids explicitly via [`Self::id`].
         #[allow(clippy::needless_pass_by_value)]
         #[inline]
         pub fn name(mut self, name: impl ToString) -> Self {
@@ -79,7 +84,8 @@ macro_rules! builder_methods_for_base {
 
         /// Sets the id of this plot item.
         ///
-        /// By default the id is determined from the name, but it can be explicitly set to a different value.
+        /// By default the id is determined from the name passed to [`Self::new`], but it can be
+        /// explicitly set to a different value.
         #[inline]
         pub fn id(mut self, id: impl Into<Id>) -> Self {
             self.base_mut().id = id.into();
