69a6f5c809bc9b49fd6e7ba33425c967425110e2
michalsustr <michal.sustr@gmail.com>
2025-11-23 13:01:36 +0100

    Inline builder_methods_for_base macro (#159)
    I found it difficult to make sure the macro is not in the public
    interface. So I just inlined it instead.

--- egui_plot/src/items/box_plot.rs
+++ egui_plot/src/items/box_plot.rs
@@ -1,4 +1,3 @@
-use crate::builder_methods_for_base;
 use crate::items::add_rulers_and_text;
 use crate::items::rect_elem::{RectElement, highlighted_color};
 use crate::{
@@ -80,7 +79,46 @@ impl BoxPlot {
         self
     }
 
-    builder_methods_for_base!();
+    /// Name of this plot item.
+    ///
+    /// This name will show up in the plot legend, if legends are turned on.
+    ///
+    /// Setting the name via this method does not change the item's id, so you can use it to
+    /// change the name dynamically between frames without losing the item's state. You should
+    /// make sure the name passed to [`Self::new`] is unique and stable for each item, or
+    /// set unique and stable ids explicitly via [`Self::id`].
+    #[expect(clippy::needless_pass_by_value)]
+    #[inline]
+    pub fn name(mut self, name: impl ToString) -> Self {
+        self.base_mut().name = name.to_string();
+        self
+    }
+
+    /// Highlight this plot item, typically by scaling it up.
+    ///
+    /// If false, the item may still be highlighted via user interaction.
+    #[inline]
+    pub fn highlight(mut self, highlight: bool) -> Self {
+        self.base_mut().highlight = highlight;
+        self
+    }
+
+    /// Allowed hovering this item in the plot. Default: `true`.
+    #[inline]
+    pub fn allow_hover(mut self, hovering: bool) -> Self {
+        self.base_mut().allow_hover = hovering;
+        self
+    }
+
+    /// Sets the id of this plot item.
+    ///
+    /// By default the id is determined from the name passed to [`Self::new`], but it can be
+    /// explicitly set to a different value.
+    #[inline]
+    pub fn id(mut self, id: impl Into<Id>) -> Self {
+        self.base_mut().id = id.into();
+        self
+    }
 }
 
 impl PlotItem for BoxPlot {
